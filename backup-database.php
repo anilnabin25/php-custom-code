28eef07729b5e7d0bebc74870341d97b080b0b0f
Divya Shrestha <divyashrestha042@gmail.com>
2021-04-09 17:21:02 +0545

    final version for the backup database

--- backup-database.php
+++ backup-database.php
@@ -5,21 +5,25 @@ ini_set('memory_limit', '-1');
 $host = "172.16.238.10";
 $user = "wwwaap";
 $pw = 'Wwwaap@123';
-$db_name = "test";
+$db_name = "wwwaap";
+
+//$host = "sddb0040315208.cgidb";
+//$user = "sddbMTcyMDgz";
+//$pw = 'NOyt$X9s';
+//$db_name = "sddb0040315208";
+
 $dsn = "mysql:host=$host;dbname=$db_name";
 
 $connect = new PDO($dsn, $user, $pw);
 $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
 $get_all_table_query = "SHOW FULL TABLES";
 $statement = $connect->prepare($get_all_table_query);
 $statement->execute();
 $result = $statement->fetchAll();
-
 $output = '';
-$data = $_GET['data'];
-foreach ($result as $table) {
+$data = isset($_GET['data']) ? $_GET['data'] : 0;
 
+foreach ($result as $table) {
   $output .= "\n\n--  ==== started " . $table[0] . " ====";
   $show_table_query = "SHOW CREATE TABLE " . $table[0] . "";
   $statement = $connect->prepare($show_table_query);
@@ -40,13 +44,7 @@ foreach ($result as $table) {
 
       for ($i = 0; $i < $table_row; $i++) {
         $single_result = $statement->fetch(PDO::FETCH_ASSOC);
-        $table_column_array = [];
-        $table_value_array = [];
-        foreach ($single_result as $key => $value) {
-          array_push($table_column_array, $key);
-          array_push($table_value_array, json_encode($value));
-        }
-
+        $table_value_array = array_values($single_result);
         $output .= "\nINSERT INTO $table[0](";
         $output .= "" . implode(", ", $column_names["column_names"]) . ") VALUES (";
         $output .= "" . implode(",", $table_value_array) . ");\n";
@@ -57,29 +55,12 @@ foreach ($result as $table) {
     foreach ($show_table_result as $show_table_row) {
       $output .= "\n\n" . $show_table_row["Create View"] . ";\n\n";
     }
-
-    //    $select_query = "SELECT * FROM " . $table[0] . "";
-    //    $statement = $connect->prepare($select_query);
-    //    $statement->execute();
-    //    $view_row = $statement->rowCount();
-    //
-    //    for ($i = 0; $i < $view_row; $i++) {
-    //      $single_result = $statement->fetch(PDO::FETCH_ASSOC);
-    //      $view_column_array = array_keys($single_result);
-    //      $view_value_array = array_values($single_result);
-    //      $output .= "\nINSERT INTO $table[0](";
-    //      $output .= "" . implode(", ", $view_column_array) . ") VALUES (";
-    //      $output .= "'" . implode("','", $view_value_array) . "');\n";
-    //    }
   }
   $output .= "\n--  ==== end " . $table[0] . " ====\n";
 }
-//print_b($output);
 
-//$mysqlExportPath = $table[0] . date("y-m-d h:i:sa") . '.sql';
 $mysqlExportPath = "backup-file" . date("y-m-d h:i:sa") . '.sql';
 print_to_file($mysqlExportPath, $output);
-//print_r("$mysqlExportPath     , <br>");
 
 
 function column_names($connect, $table_name)
@@ -94,7 +75,6 @@ function column_names($connect, $table_name)
     $single_result = $statement->fetch(PDO::FETCH_ASSOC);
     array_push($column_names_with_quote, "QUOTE(" . $single_result["Field"] . ")");
     array_push($column_names, $single_result["Field"]);
-
   }
   return ["column_names_with_quote" => $column_names_with_quote, "column_names" => $column_names];
 }
